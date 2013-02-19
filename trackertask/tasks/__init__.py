2055f3595591f6c18d0ab7e88fe3f1b4e1a98911
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2013-02-19 12:13:50 +0100

    Found URL class for Sqlalchemy db url. This URL class can be questioned to make a an db connection in another language like jdbc.
    For PythonTask make self.db connect to db and perform reflection.
    So it can be queried using orm without declaring all tables by hand.
    
    Make subprocess take environment for alternative to pass things to executable and change it to shell=True to be platform independent.

--- trackertask/tasks/__init__.py
+++ trackertask/tasks/__init__.py
@@ -3,6 +3,11 @@ import sys
 import subprocess
 from celery import Task
 from celery import current_app
+from sqlalchemy.engine.url import make_url
+from sqlalchemy import engine_from_config
+from sqlalchemy import MetaData
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import scoped_session
 from rpy2.robjects.packages import SignatureTranslatedAnonymousPackage
 from oct2py import octave
 
@@ -20,9 +25,77 @@ class PythonTask(Task):
     _db = None
 
     @property
+    def db_url(self):
+        """Instance of :class:`~sqlalchemy.engine.url.URL`
+
+        Can be used to create database connection in another language by using URL's properties.
+
+        eg. For Matlab the SQLAlchemy URL has to be converted to JDBC
+        In Python::
+
+            u = self.db_url
+            username = u.username
+            password = u.password
+            instance = u.database
+            drivers = {'postgresql': 'org.postgresql.Driver'}
+            driver = drivers[u.drivername]
+            jdbc_url = 'jdbc:{drivername}://{host}:{port}/{database}'.format(drivername=u.drivername,
+                                                                             host=u.host,
+                                                                             port=u.port or 5432,
+                                                                             database=u.database)
+            if u.query.has_key('sslmode') and u.query['sslmode'] == 'require':
+                jdbc_url += '?ssl=true'
+
+        In Matlab::
+
+            conn = database(instance, username, password, driver, jdbc_url)
+
+
+        eg. For R
+        In Python::
+            u = self.db_url
+            drivers = {'postgresql': 'PostgreSQL'}
+            driver = drivers[u.drivername]
+            dbname=u.database
+            host = u.host
+            port = u.port or 5432
+            username = u.username
+            password = u.password
+
+        In R::
+            drv <- dbDriver(driver)
+            # TODO how to use sslmode=require in R, possibly via PGSSLMODE environment variable?
+            con <- dbConnect(drv, dbname=dbname, host=host, port=port, user=username, password=password)
+
+        """
+        return make_url(current_app.conf['sqlalchemy.url'])
+
+    @property
     def db(self):
+        """Connects to database and performs reflection on all available tables.
+
+        Uses current_app.conf for sqlalchemy.* configuration.
+        Performs reflection on default schema or if 'sqlalchemy.schema' config is set reflects on that schema.
+
+        Returns tuple with :class:`sqlalchemy.orm.session.Session` and :class:`sqlalchemy.MetaData`.
+
+        Usage example::
+
+            Session, meta = self.db
+            tracking = meta.tables['gps.uva_tracking_speed']
+            results = Session().query(tracking).all()
+
+        """
         if self._db is None:
-            self._db = current_app.conf['sqlalchemy.url']
+            engine = engine_from_config(current_app.conf)
+            Session = scoped_session(sessionmaker(bind=engine))
+
+            meta = MetaData()
+            if current_app.conf.has_key('sqlalchemy.schema'):
+                meta.reflect(bind=engine, schema=current_app.conf['sqlalchemy.schema'])
+            else:
+                meta.reflect(bind=engine)
+            self._db = (Session, meta,)
         return self._db
 
     @property
@@ -98,9 +171,21 @@ class SubProcessTask(PythonTask):
         """Arguments prepended to run(*args) which are used as Popen args"""
         return []
 
+    def env(self):
+        """Environment to use for subprocess.
+
+        Defaults to current environment.
+
+        Can be used to pass sensitive information to subprocess like database passwords.
+        """
+        return os.environ
+
     def run(self, *args, **kwargs):
         pargs = self.pargs() + args
         popen = subprocess.Popen(pargs,
+                                 cwd=self.task_dir,
+                                 shell=True,
+                                 env=self.env,
                                  stderr=subprocess.PIPE,
                                  stdout=subprocess.PIPE,
                                  )
@@ -145,8 +230,7 @@ class MatlabTask(PythonTask):
 
     def pargs(self):
         """Prepend the deployment script and matlab location"""
-        return ['/bin/sh',
-                os.path.join(self.task_dir, self.deploy_script),
+        return [os.path.join(self.task_dir, self.deploy_script),
                 self.matlab,
                 ]
 
