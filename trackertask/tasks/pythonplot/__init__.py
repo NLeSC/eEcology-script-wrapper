21e8582b2f57b0cd26f35cc7b0059d2147d99d5c
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2013-02-21 10:23:15 +0100

    Perform db query using task arguments start/end and tracker ids.

--- trackertask/tasks/pythonplot/__init__.py
+++ trackertask/tasks/pythonplot/__init__.py
@@ -1,10 +1,16 @@
 import time
 import json
 import os
+import datetime
 from celery import Task
 from celery import current_task
 from celery import current_app
+from celery.utils.log import get_task_logger
+from sqlalchemy import func
 from trackertask.tasks import PythonTask
+import trackertask.models as models
+
+logger = get_task_logger(__name__)
 
 class PythonPlotTask(PythonTask):
     name = "pythonplot"
@@ -13,7 +19,19 @@ class PythonPlotTask(PythonTask):
 
     """Perform a simple python task"""
     def run(self, start, end, trackers, username, password):
+        ids = [tracker['id'] for tracker in trackers]
         msg = 'fancy plot of {} from {} to {}'.format(json.dumps(trackers), start, end)
+        s = models.DBSession()
+        tr = models.meta.tables['gps.uva_tracking_limited']
+        tid = tr.columns.device_info_serial
+        dt = tr.columns.date_time
+        q = s.query(tid, func.count(tid))
+        q = q.filter(tid.in_(ids))
+        q = q.filter(dt.between(start, end))
+        q = q.group_by(tid)
+        r = q.all()
+        msg += json.dumps(r)
+
         fn = os.path.join(self.output_dir, 'plot.txt')
         with open(fn, 'w') as f:
             f.write(msg)
@@ -22,10 +40,13 @@ class PythonPlotTask(PythonTask):
                 }
 
     def formfields2taskargs(self, fields):
-        return {'start': fields['start'],
-                'end': fields['end'],
+        return {'start': iso8601parse(fields['start']),
+                'end': iso8601parse(fields['end']),
                 'trackers': fields['trackers'],
                 # below example of adding argument values
                 'username': 'someuser',
                 'password': 'somepw',
                 }
+
+def iso8601parse(datetime_string):
+    return datetime.datetime.strptime(datetime_string, "%Y-%m-%dT%H:%M:%S"),
