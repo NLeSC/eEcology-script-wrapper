25749b2f48234b83749da946899d979048c213f9
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2014-01-02 12:05:37 +0100

    gpxdata is missing python_dateutil requirement. Added as requirement for script wrapper.

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ requires = [
     'rst2pdf',  # to create PDF of documentation
     'sphinx_bootstrap_theme',  # to create nice looking html documentation
     'simplekml',  # for pykml task
+    'python_dateutil', # requirement for gpxdata
     'gpxdata', # for gpx task
 ]
 
